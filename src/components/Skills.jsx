8780ba638b4c22cef7bad5652b02239b9e2e4a17
hxtien1132 <78005808+hxtien1132@users.noreply.github.com>
2024-03-02 13:08:11 +0700

    update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,7 +10,7 @@ import javascript from '../Assets/icons8-javascript-480.png'
 import mongodb from '../Assets/icons8-mongodb-480.png'
 import materialUi from '../Assets/icons8-material-ui-480.png'
 import php from '../Assets/php.png'
-
+import java from '../Assets/icons8-spring-boot-480.png'
 export default function Projects() {
   const techs = [
     { id: 1, src: html, title: 'HTML', style: 'shadow-orange-500' },
@@ -23,8 +23,8 @@ export default function Projects() {
     { id: 8, src: nodejs, title: 'NODEJS', style: 'shadow-green-500' },
     { id: 9, src: sql, title: 'SQL', style: 'shadow-red-500' },
     { id: 10, src: github, title: 'GITHUB', style: 'shadow-slate-500' },
-    { id: 11, src: mongodb, title: 'MongoDB', style: 'shadow-orange-500' },
-    { id: 12, src: php, title: 'PHP', style: 'shadow-blue-500' }
+    { id: 11, src: mongodb, title: 'MongoDB', style: 'shadow-orange-500' }
+    // { id: 12, src: php, title: 'PHP', style: 'shadow-blue-500' }
   ]
   return (
     <div name='experience' className='w-full md:min-h-screen bg-gradient-to-b from-gray-800 to-black'>
